6a62c36a561a52c8eb79896a79926e9f5e4ff54a
Kevin Schmitz <kfschmitz@gmail.com>
2018-10-09 17:01:48 -0700

    added style

--- src/components/doctorinfo.js
+++ src/components/doctorinfo.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { connect } from "react-redux";
 import store from "../store";
+import "./doctorinfo.css";
+import "../grid.css";
 
 export const RenderDocData = props => {
   console.log({ props });
@@ -14,41 +16,47 @@ export const RenderDocData = props => {
   console.log(data, "this is my data");
 
   return (
-    <div className="doc-container">
-      <ul>
-        {data.map(item => (
-          <li key={item.uid}>
-            <h2>{item.name}</h2>
-            <div>
-              {item.doctors.map(item => (
+    <section>
+      <div className="row">
+        <div className="col-5">
+          <div className="doc-container">
+            <ul>
+              {data.map(item => (
                 <li key={item.uid}>
-                  <h4>
-                    {item.profile.first_name} {item.profile.last_name},{" "}
-                    {item.profile.title}
-                  </h4>
-                  <p>NPI: {item.npi}</p>
-                </li>
-              ))}
-            </div>
+                  <h2>{item.name}</h2>
+                  <div>
+                    {item.doctors.map(item => (
+                      <li key={item.uid}>
+                        <h4>
+                          {item.profile.first_name} {item.profile.last_name},{" "}
+                          {item.profile.title}
+                        </h4>
+                        <p>NPI: {item.npi}</p>
+                      </li>
+                    ))}
+                  </div>
 
-            <p>{item.visit_address.street}</p>
-            <p>
-              {item.visit_address.city}, {item.visit_address.state_long},{" "}
-              {item.visit_address.zip}
-            </p>
-            <div>
-              {item.phones.map(item => (
-                <li key={item.uid}>
+                  <p>{item.visit_address.street}</p>
                   <p>
-                    {item.type}: {item.number}
+                    {item.visit_address.city}, {item.visit_address.state_long},{" "}
+                    {item.visit_address.zip}
                   </p>
+                  <div>
+                    {item.phones.map(item => (
+                      <li key={item.uid}>
+                        <p>
+                          {item.type}: {item.number}
+                        </p>
+                      </li>
+                    ))}
+                  </div>
                 </li>
               ))}
-            </div>
-          </li>
-        ))}
-      </ul>
-    </div>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
